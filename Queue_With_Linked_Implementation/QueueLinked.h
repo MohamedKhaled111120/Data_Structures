ba8a9c748e06397811d2d169edd595292a4c502e
Mohamed Khaled <81655815+MohamedKhaled111120@users.noreply.github.com>
2023-08-05 08:15:42 +0300

    Queue with Linked implementation has been uploaded

---
+++ Queue_With_Linked_Implementation/QueueLinked.h
@@ -0,0 +1,21 @@
+//user should define this elements;
+#define QueueEntry int // for example
+typedef struct queuenode {
+	QueueEntry entry;
+	struct queuenode* next;
+}QueueNode;
+
+typedef struct queue {
+	QueueNode* front;
+	QueueNode* rear;
+	int size;
+}Queue;
+
+void InitQueue(Queue* pq); //frun to init the queue
+void Append(Queue* pq, QueueEntry e); // append(add) element  in queue
+void Serve(QueueEntry* pe, Queue* pq); // serve (take) elemnt from queue
+int QueueEmpty(Queue* pq); // ckeck if queue is empty? 
+int QueueFull(Queue* pq); // check if queue is full
+int QueueSize(Queue* pq); // return the queue size
+void ClearQueue(Queue* pq); // clear the queue
+void TraverseQueue(Queue* pq, void(*pf)(QueueEntry)); // take fun from user in make it loop to each element in queue
