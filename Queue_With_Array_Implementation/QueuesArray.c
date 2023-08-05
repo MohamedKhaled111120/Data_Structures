92089a996d1b5ca7cdfd9061dc08d56b3df899b6
Mohamed Khaled <81655815+MohamedKhaled111120@users.noreply.github.com>
2023-08-05 16:03:46 +0300

    Queue implementation has been uploaded

---
+++ Queue_With_Array_Implementation/QueuesArray.c
@@ -0,0 +1,59 @@
+#include"QueuesArray.h"
+
+void InitQueue(Queue* pq)
+{
+	pq->front = 0;
+	pq->rear = -1;
+	pq->size = 0;
+}
+
+void Append(QueueEntry e, Queue* pq)
+{
+	if (pq->rear==MAXQUEUE)
+	{
+		pq->rear = 0;
+	}
+	else
+	{
+		pq->rear++;
+	}
+	//OR
+	//pq->rear = (pq->rear + 1) % MAXQUEUE;
+	pq->entry[pq->rear] = e;
+	pq->size++;
+}
+void Serve(QueueEntry* pe, Queue* pq)
+{
+	*pe = pq->entry[pq->front];
+	pq->front = (pq->front + 1) % MAXQUEUE;
+	pq->size--;
+}
+int QueueEmpty(Queue* pq)
+{
+	return (!pq->size);
+}
+
+int QueueFull(Queue* pq)
+{
+	return (pq->size == MAXQUEUE);
+}
+
+int QueueSize(Queue* pq)
+{
+	return pq->size;
+}
+void ClearQueue(Queue* pq)
+{
+	pq->front = 0;
+	pq->rear = -1;
+	pq->size = 0;
+}
+void TraverseQueue(Queue* pq, void(*pf)(QueueEntry))
+{
+	int pos, s;
+	for (pos = pq->front, s = 0; s < pq->size; s++)
+	{
+		(*pf)(pq->entry[pos]);
+		pos = (pos + 1) % MAXQUEUE;
+	}
+}
